9254969524742f786b12a92235fa93ecc098f258
BattleProgrammerCadu <gabrielgualbertooliveira@gmail.com>
2022-01-16 06:04:29 -0300

    feat bot ta tocando as porra das musica agr

--- src/marquin.js
+++ src/marquin.js
@@ -1,43 +1,82 @@
 require('dotenv').config();
 
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_SCHEDULED_EVENTS] });
+const Discord = require("discord.js");
+const Distube = require("distube").default;
+const config = require("../config.json");
 
-client.login(process.env.BOT_TOKEN);
+const client = new Discord.Client({
+  intents: 641,
+});
 
-const wallaceQuotes = ["Alá o cara cago em pé no terminal sao matheuskkkkkkkkkkkkkkj",
-                       "O pedreiro viu o cara cagando ahahahah", 
-                       "E o dia que ele escondeu a trakinas hein esse foi foda", 
-                       "Estrago as chance dele pq derrubou uma prateleira de doritos", 
-                       "Lua vai iluminar os pensamentos dela",
-                       "Tiramo 9,5 no tcc fodase"];
+const distube = new Distube(client, {
+    emitNewSongOnly: false,
+    searchSongs: 0,
+  });
 
-client.on("ready", () => {
+
+client.once("ready", () => {
     console.log("Oi Susan, tá");
 });
+
+distube.on("playSong", (queue, song) => {
+    let playembed = new Discord.MessageEmbed()
+      .setTitle(`tocando `)
+      .setThumbnail(song.thumbnail)
+      .setDescription(`[${song.name}](${song.url})`)
+      .addField("duracao", `${song.formattedDuration.toString()}`, true)
+    queue.textChannel.send({ embeds: [playembed] });
+  });
+  distube.on("addSong", (queue, song) => {
+    let playembed = new Discord.MessageEmbed()
+      .setTitle(`adicionei na playlist `)
+      .setThumbnail(song.thumbnail)
+      .setDescription(`[${song.name}](${song.url})`)
+      .addField("duracao", `${song.formattedDuration.toString()}`, true)
+        ;
+  
+    queue.textChannel.send({ embeds: [playembed] });
+  });
+  distube.on('addList', (queue, plalist) => {
+    let playembed = new Discord.MessageEmbed()
+      .setTitle(`bota o amendoim no buraco do amendoim`)
+      .setThumbnail(plalist.thumbnail)
+      .setDescription(`[${plalist.name}](${plalist.url})`)
+      .addField("duracao", `${plalist.formattedDuration.toString()}`, true)
+     ;
+  
+    queue.textChannel.send({ embeds: [playembed] });
+  })
+
+
 
 client.on("guildCreate", guild=>{
     guild.systemChannel.send('Bota na cabeça que estilo num é marra');
 });
 
-client.on("messageUpdate", msg => {
-    msg.reply(`Eae ${msg.author} ta editando né pilantra`);
-})
+client.on("messageCreate", async(msg)=>{ 
+    if( !msg.guild || msg.author.bot || !msg.content.startsWith(config.prefix)) return;
 
-client.on("messageDelete", msg => {
-    msg.channel.send(`Eae ${msg.author} ta apagando as mensagens pq seu comedia!?!`);
-})
-
-client.on("messageCreate", async(msg)=>{
-    if (msg.content === "bao?") {
-        msg.channel.send("aoba ces tao bao?");
-    }
- 
-    if(msg.content === '!luvas'){
+    let args = msg.content.slice(config.prefix.length).split(" ");
+    let cmd  = args.shift()?.toLowerCase();
+    
+    if(cmd === 'luvas'){
         msg.channel.send(`Já lavo o pinto hj luvas? <@349736021115666445>`);
     }
-    if(msg.content === '!wallace'){
+    else if(cmd === 'wallace'){
         let quote = Math.floor(Math.random() * wallaceQuotes.length);
         msg.channel.send(`${wallaceQuotes[quote]}`);
+    }else if(cmd === 'play'){
+        let search = args.join(" ");
+        let channel = msg.member.voice.channel;
+        let queue = distube.getQueue(msg.guildId);
+        if(!channel){
+            return msg.reply('Entra num canal de audio antes né o burro');
+        }
+        if(!search){
+            return msg.reply('Fala direito caralho');
+        }
+        distube.play(msg, search);
     }
-});
+});  
+
+client.login(process.env.BOT_TOKEN);
